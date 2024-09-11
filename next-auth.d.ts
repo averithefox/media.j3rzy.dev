3238c5d368028b8df991b881d7ed7d128d7a6aca
realcatirl <github@j3rzy.dev>
2024-09-11 14:58:15 -0400

    Added cn()

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -7,17 +7,17 @@ export type ExtendedUser = DefaultSession["user"] & {
 }
 
 declare module "next-auth"
-  {
+{
   interface Session
-    {
+  {
     user: ExtendedUser;
-    }
   }
+}
 
 declare module "@auth/core/jwt"
-  {
+{
   interface JWT
-    {
+  {
     role?: UserRole;
-    }
   }
+}
