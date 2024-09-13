5018c5af8269692618b9451c38eee6cb8f4aebb2
0xe0b69e <github@j3rzy.dev>
2024-09-14 00:20:58 +0200

    Improvements

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { useSession } from "next-auth/react";
 import { IoTrashBin } from "react-icons/io5";
+import { FaLink } from "react-icons/fa";
+import { BiCopy } from "react-icons/bi";
 
 interface File
 {
@@ -69,12 +71,27 @@ export default function Page ()
         {files.filter(file => searchQuery ? new RegExp(searchQuery, "img").test(file.name) : true).map((file, i) =>
           (
             <div className="relative overflow-hidden group rounded-md">
-              {session.data?.user.role === "ADMIN" && (
-                <div
-                  className="absolute top-0 -translate-y-16 group-hover:translate-y-0 transition-all duration-250 w-full flex flex-row space-x-1 justify-center bg-black/50">
-                  <div className="flex-grow" />
+              <div
+                className="absolute top-0 -translate-y-16 group-hover:translate-y-0 transition-all duration-250 w-full flex flex-row justify-center bg-black/0">
+                <div className="flex-grow"/>
+                <button
+                  className="p-1 bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700"
+                  onClick={async () => await navigator.clipboard.writeText(`${location.origin}/media/${file.name}`)}
+                  aria-label="Copy"
+                >
+                  <BiCopy className="text-white"/>
+                </button>
+                <Link
+                  href={`/media/${file.name}`}
+                  target="_blank"
+                  className="cursor-pointer p-1 bg-blue-600 hover:bg-blue-700 active:bg-blue-800"
+                  aria-label="Open in new tab"
+                >
+                  <FaLink className="text-white"/>
+                </Link>
+                {session.data?.user.role === "ADMIN" && (
                   <button
-                    className="p-1 bg-red-500 hover:bg-red-600"
+                    className="p-1 bg-red-500 hover:bg-red-600 active:bg-red-700"
                     onClick={async () =>
                     {
                       const res = await fetch("/files", {
@@ -88,31 +105,26 @@ export default function Page ()
                       if ( json.success )
                         setFiles((files) => files.filter((f) => f.name !== file.name));
                       else alert(json.error);
-                    }}>
-                    <IoTrashBin className="text-white" />
+                    }}
+                    aria-label="Delete"
+                  >
+                    <IoTrashBin className="text-white"/>
                   </button>
-                </div>
-              )}
+                )}
+              </div>
               {(() =>
               {
-                switch ( file.type.split("/")[0] )
+                switch ( file.type === "application/x-javascript" ? "text" : file.type.split("/")[0] )
                 {
                   case "image":
-                    return <Link
-                      href={`/${file.name}`}
-                      target="_blank"
-                      className="cursor-pointer"
-                      key={i}
-                    >
-                      <Image
-                        src={`/media/${file.name}`}
-                        alt={file.name}
-                        width={200}
-                        height={200}
-                        className="w-auto h-auto"
-                        unoptimized={file.type === "image/gif"}
-                      />
-                    </Link>;
+                    return <Image
+                      src={`/media/${file.name}`}
+                      alt={file.name}
+                      width={200}
+                      height={200}
+                      className="w-auto h-auto"
+                      unoptimized={file.type === "image/gif"}
+                    />;
                   case "video":
                     return <video
                       controls
@@ -124,32 +136,29 @@ export default function Page ()
                       Your browser does not support the video element.
                     </video>;
                   case "audio":
-                    return <audio controls key={i} className="w-[200px]">
-                      <source src={`/media/${file.name}`} type={file.type}/>
-                      Your browser does not support the audio element.
-                    </audio>;
+                    return <div className="w-[200px] h-[200px] flex items-center justify-center">
+                      <audio controls key={i} className="w-[200px]">
+                        <source src={`/media/${file.name}`} type={file.type}/>
+                        Your browser does not support the audio element.
+                      </audio>
+                    </div>;
                   case "text":
-                    return <Link
-                      href={`/media/${file.name}`}
-                      target="_blank"
-                      className="cursor-pointer"
-                      key={i}
-                    >
-                      <iframe
-                        src={`/media/${file.name}`}
-                        width={200}
-                        height={200}
-                      />
-                    </Link>;
+                    return <iframe
+                      src={`/media/${file.name}`}
+                      width={200}
+                      height={200}
+                    />;
                   default:
-                    return <Link
-                      href={`/media/${file.name}`}
-                      target="_blank"
-                      className="cursor-pointer"
-                      key={i}
-                    >
-                      <p className="text-center">{file.name} ({file.type})</p>
-                    </Link>;
+                    return <div className="w-[200px] h-[200px] flex items-center justify-center">
+                      <Link
+                        href={`/media/${file.name}`}
+                        target="_blank"
+                        className="cursor-pointer"
+                        key={i}
+                      >
+                        <p className="text-center text-blue-600 underline">{file.name} ({file.type})</p>
+                      </Link>
+                    </div>;
                 }
               })()}
             </div>
