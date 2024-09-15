6a97fd9b0cce1d7a378edaf59444bb96cf793976
0xe0b69e <github@j3rzy.dev>
2024-09-15 13:18:20 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 git pull
 
-sudo chown -R $USER:$USER /mnt
+uploads_dir=$(grep -A 6 'uploads_data:' docker-compose.yml | grep 'device:' | sed 's/.*device: //')
+
+if [ -n "$uploads_dir" ]; then
+  if [ -d "$uploads_dir" ]; then
+    if [ ! -r "$uploads_dir" ] || [ ! -w "$uploads_dir" ]; then
+      sudo chmod u+rw "$uploads_dir"
+    fi
+  else
+    if ! mkdir -p "$uploads_dir"; then
+      sudo mkdir -p "$uploads_dir"
+    fi
+  fi
+fi
 
 if ! docker ps --format '{{.Names}}' | grep -qE 'mediaj3rzydev_nextjs_1|mediaj3rzydev_db_1'; then
   docker-compose up -d
