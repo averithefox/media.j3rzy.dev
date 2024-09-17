b662238424937b0ce53548c659e4a8c37727591b
realcatirl <github@j3rzy.dev>
2024-09-17 07:23:28 -0400

    Fixed /files returning localhost url

--- app/files/route.ts
+++ app/files/route.ts
@@ -7,7 +7,7 @@ import * as fs from "node:fs/promises";
 import path from "node:path";
 import { fileTypeFromBuffer, FileTypeResult } from "file-type";
 import { cache } from "react";
-import { NextURL } from "next/dist/server/web/next-url";
+import { metadata } from "@/app/layout";
 
 const getFileRecords: () => Promise<FileRecord[]> = cache(async (): Promise<FileRecord[]> => db.file.findMany());
 const getFileRecordByFilename: ( filename: string ) => Promise<FileRecord | null> = cache(async ( filename: string ): Promise<FileRecord | null> => db.file.findUnique({ where: { filename } }));
@@ -41,21 +41,21 @@ async function fileExists( filePath: string ): Promise<boolean>
   }
 }
 
-const getFileObject = ( { mimeType, filename }: { mimeType: string, filename: string }, nextUrl: NextURL ) => ({
+const getFileObject = ( { mimeType, filename }: { mimeType: string, filename: string } ) => ({
   name: filename,
   type: mimeType,
-  rawUrl: `${new URL(`/raw/${encodeURI(filename)}`, nextUrl).toString()}`,
-  url: `${new URL(`/${encodeURI(filename)}`, nextUrl).toString()}`,
+  rawUrl: `${new URL(`/raw/${encodeURI(filename)}`, metadata.metadataBase!).toString()}`,
+  url: `${new URL(`/${encodeURI(filename)}`, metadata.metadataBase!).toString()}`,
 });
 
-export async function GET( req: NextRequest )
+export async function GET()
 {
   try
   {
     const fileRecords: FileRecord[] = await getFileRecords();
     return Response.json({
       success: true,
-      data: fileRecords.map(record => getFileObject(record, req.nextUrl)),
+      data: fileRecords.map(getFileObject),
     });
   } catch ( e: any )
   {
@@ -119,7 +119,7 @@ export async function POST( req: NextRequest )
     
     return Response.json({
       success: true,
-      data: uniqueData.map(record => getFileObject(record, req.nextUrl)),
+      data: uniqueData.map(getFileObject),
     });
   } catch ( e: any )
   {
