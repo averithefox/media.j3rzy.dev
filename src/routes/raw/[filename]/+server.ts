d4139a709d4a113411d48a3d23efd82787a3a569
Averi <github@j3rzy.dev>
2024-09-28 11:57:33 +0200

    Added the /raw route

---
+++ src/routes/raw/[filename]/+server.ts
@@ -0,0 +1,36 @@
+import type { RequestEvent } from "@sveltejs/kit";
+import { db } from "$lib/server/db";
+import * as path from "node:path";
+
+export async function GET (req: RequestEvent)
+{
+  const { filename: pathname } = req.params;
+  
+  if (!pathname)
+    return Response.json({ success: false, error: "I'm a teapot" }, { status: 418 })
+  
+  try
+  {
+    const filename = decodeURIComponent(pathname);
+    const record = await db.file.findUnique({ where: { filename } });
+    
+    if ( !record )
+      return Response.json({ success: false, error: "File not found", at: "database" }, { status: 404 });
+    
+    const file = Bun.file(path.join(process.cwd(), "uploads", record.hash));
+    
+    if (!await file.exists())
+      return Response.json({ success: false, error: "File not found", at: "filesystem" }, { status: 404 });
+    
+    return new Response(file.stream(), {
+      headers: {
+        "Content-Type": record.mimeType,
+        "X-File-Hash": record.hash,
+        "Content-Length": file.size.toString(),
+      },
+    });
+  } catch ( e: any )
+  {
+    return Response.json({ success: false, error: e.message }, { status: 500 })
+  }
+}
