f12a23839d73623ca3e7d35b548ea8f305135910
Averi <github@j3rzy.dev>
2024-09-28 12:16:30 +0200

    Added authjs

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { DefaultSession } from "@auth/sveltekit";
+import type { UserRole } from "@prisma/client";
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -10,4 +13,23 @@ declare global {
 	}
 }
 
-export {};
+// AuthJS stuff
+export type ExtendedUser = DefaultSession["user"] & {
+	role: UserRole;
+}
+
+declare module "@auth/sveltekit"
+{
+	interface Session
+	{
+		user: ExtendedUser;
+	}
+}
+
+declare module "@auth/core/jwt"
+{
+	interface JWT
+	{
+		role?: UserRole;
+	}
+}
