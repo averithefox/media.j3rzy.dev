d22a685c80327062b52c00c440dfb731baf18268
realcatirl <github@j3rzy.dev>
2024-09-29 11:13:27 +0200

    Hopefully fixed redirect issues

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,19 @@
-export { handle } from "./auth";
+import type { Handle } from "@sveltejs/kit";
+import { building } from "$app/environment";
+import { handle as authHandle } from "./auth";
+import { sequence } from "@sveltejs/kit/hooks";
+
+const handleProtocol: Handle = async ({ event, resolve }) =>
+{
+  if ( !building )
+  {
+    const protocol = event.request.headers.get("x-forwarded-proto") || "http";
+    const host = event.request.headers.get("host") || "localhost";
+    event.url.protocol = `${protocol}:`;
+    event.url.host = host.split(":")[0];
+  }
+  
+  return resolve(event);
+};
+
+export const handle: Handle = sequence(handleProtocol, authHandle);
