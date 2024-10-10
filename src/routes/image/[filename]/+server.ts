3b3593ace0dfb255d034d895e27a879968d91753
Averi <github@j3rzy.dev>
2024-10-10 08:46:57 -0400

    Made /image awalys return file (or json on error)

--- src/routes/image/[filename]/+server.ts
+++ src/routes/image/[filename]/+server.ts
@@ -10,16 +10,17 @@ export const GET: RequestHandler = async ( event ) =>
   if ( response.status !== 200 )
     return response;
   
-  const validSizes: number[] = [200, 300, 400];
+  const validSizes: number[] = [ 200, 300, 400 ];
   
   const buffer = Buffer.from(await (await response.blob()).arrayBuffer());
-  const w = new URL(event.request.url).searchParams.get("w");
-  let width = w ? (!isNaN(Number(w)) && Number.isInteger(Number(w)) ? Number(w) : 200) : 200;
-  width = validSizes.includes(width) ? width : validSizes.reduce((prev, curr) => Math.abs(curr - width) < Math.abs(prev - width) ? curr : prev);
   const mimeType = response.headers.get("Content-Type")!;
   
   if ( mimeType.split("/")[0] !== "image" || mimeType.split("/")[1] === "gif" )
-    return Response.json({ success: false, error: "Invalid file type" }, { status: 400 });
+    return new Response(buffer, { headers: response.headers });
+  
+  const w = new URL(event.request.url).searchParams.get("w");
+  let width = w ? (!isNaN(Number(w)) && Number.isInteger(Number(w)) ? Number(w) : 200) : 200;
+  width = validSizes.includes(width) ? width : validSizes.reduce(( prev, curr ) => Math.abs(curr - width) < Math.abs(prev - width) ? curr : prev);
   
   const resultBuffer = await sharp(buffer).resize(width).toFormat("webp").toBuffer();
   return new Response(resultBuffer, {
