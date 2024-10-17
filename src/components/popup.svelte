534b21eaca42aa481f4938ce07a13cab70ed508d
Averi <github@j3rzy.dev>
2024-10-17 10:03:40 -0400

    Improvements

--- src/components/popup.svelte
+++ src/components/popup.svelte
@@ -9,29 +9,29 @@
   let closeTimeout: ReturnType<typeof Timer> | null = null; // As TimerClass isn't exported
 
   $: percentCompleted = 0;
-  const updateInterval = setInterval(() => percentCompleted = closeTimeout?.getCompletedPercentage() ?? 0, 50);
+  const updateInterval = setInterval(() => percentCompleted = Math.floor(closeTimeout?.getCompletedPercentage() ?? 0), 50);
 
-  const close = () =>
+  const close = function()
   {
     popupStore.set(null);
     closeTimeout = null;
-    percentCompleted = 0;
   };
 
-  popupStore.subscribe(( value ) =>
+  popupStore.subscribe(function( value )
   {
     active = !!value;
     setTimeout(() => data = value, value ? 0 : 300);
-    if ( value ) closeTimeout = Timer(() => popupStore.set(null), 5000);
+    if ( value )
+    {
+      percentCompleted = 0;
+      closeTimeout = Timer(close, 5000);
+    }
   });
 
   const controller = new AbortController();
 
-  onMount(() => window.addEventListener("keydown", ( event ) =>
-  {
-    if ( active && event.key === "Escape" ) close();
-  }, { signal: controller.signal }));
-  onDestroy(() =>
+  onMount(() => window.addEventListener("keydown", ( event ) => active && event.key === "Escape" ? close() : null, controller));
+  onDestroy(function()
   {
     controller.abort();
     clearInterval(updateInterval);
@@ -40,16 +40,17 @@
 
 <div
   data-active="{active}"
-  class="data-[active=false]:opacity-0 data-[active=false]:pointer-events-none fixed inset-0 flex items-center justify-center transition-all duration-300 data-[active=true]:z-[9999] data-[active=false]:-z-[9999]"
+  class="data-[active=false]:scale-0 data-[active=false]:pointer-events-none fixed inset-0 flex items-center justify-center transition-all duration-300 data-[active=true]:z-[9999] data-[active=false]:-z-[9999]"
   id="popup"
 >
   <div
-    class="rounded-md flex flex-col w-[350px] h-[200px] bg-red-600"
+    class="rounded-sm flex flex-col w-[350px] h-[200px] bg-[#333333] shadow-md"
     on:mouseenter={() => closeTimeout?.pause()}
     on:mouseleave={() => closeTimeout?.resume()}
     role="dialog"
   >
-    <div class="p-1 flex items-center justify-between">
+    <div
+      class="p-1 flex items-center justify-between bg-gradient-to-b from-90% from-[#2f2f2f] to-[#333333] overflow-hidden">
       <h1 class="font-bold capitalize">{data?.title}</h1>
       <button on:click={close}>
         <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -62,6 +63,10 @@
         </svg>
       </button>
     </div>
-    <span class="h-1 bg-white" id="progress" style="width: {percentCompleted}%;" />
+    <span
+      class="h-1 bg-[#84cb94]"
+      id="progress"
+      style="width: {percentCompleted}%;"
+    />
   </div>
 </div>
